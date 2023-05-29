8f6bb59437c93ea26721fb75dbd6c3d1d95453bc
Maxim <MaximKomoza@yandex.ru>
2023-05-30 00:09:11 +0700

    difficulty window

---
+++ components/difficulty/difficulty-component.js
@@ -0,0 +1,13 @@
+export const difficultyPage = () => {
+  return (`
+  <div class="difficulty">
+    <h2 class="difficulty__text">Выбери сложность</h2>
+    <ul class="difficulty__value-wrap">
+        <li class="difficulty__value">1</li>
+        <li class="difficulty__value">2</li>
+        <li class="difficulty__value">3</li>
+    </ul>
+    <button class="difficulty__start">Старт</button>
+  </div>
+`);
+};
