5c91b7533ac0ab2a25e4e154d2989a0e6f1e3a34
Maxim <MaximKomoza@yandex.ru>
2023-05-30 08:09:11 +0700

    change list value to input radio

--- components/difficulty/difficulty-component.js
+++ components/difficulty/difficulty-component.js
@@ -1,13 +1,24 @@
 export const difficultyPage = () => {
-  return (`
-  <div class="difficulty">
+  return `
+    <div class="difficulty">
     <h2 class="difficulty__text">Выбери сложность</h2>
-    <ul class="difficulty__value-wrap">
-        <li class="difficulty__value">1</li>
-        <li class="difficulty__value">2</li>
-        <li class="difficulty__value">3</li>
-    </ul>
-    <button class="difficulty__start">Старт</button>
+    <form class="difficulty__form">
+      <div class="difficulty__value-wrap">
+        <label class="difficulty__value">
+          <input type="radio" name="difficulty" value="1">
+          <span>1</span>
+        </label>
+        <label class="difficulty__value">
+          <input type="radio" name="difficulty" value="2">
+          <span>2</span>
+        </label>
+        <label class="difficulty__value">
+          <input type="radio" name="difficulty" value="3">
+          <span>3</span>
+        </label>
+      </div>
+      <button class="difficulty__start">Старт</button>
+    </form>
   </div>
-`);
+`;
 };
